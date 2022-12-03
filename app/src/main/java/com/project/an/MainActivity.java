d460095238983304405e11cab785c73fc9de81aa
udith355 <udith.19@cse.mrt.ac.lk>
2022-12-03 17:47:59 +0530

    video capture part

--- app/src/main/java/com/project/an/MainActivity.java
+++ app/src/main/java/com/project/an/MainActivity.java
@@ -2,7 +2,9 @@ package com.project.an;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void uploadbtnPress(View view) {
+        Intent intent = new Intent(this, UploadActivity.class);
+        startActivity(intent);
+    }
+
+    public void watchbtnPress(View view) {
+        Intent intent = new Intent(this, WatchVideosActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/project/an/PlayVideoActivity.java
@@ -0,0 +1,42 @@
+package com.project.an;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.net.URI;
+
+public class PlayVideoActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+    private Uri videoUri;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.videoplayer);
+        //get video view by id
+        videoView = findViewById(R.id.playVideo);
+        //set media controllers
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        Intent intent = getIntent();
+        if(intent != null && intent.hasExtra("VIDEO_URI")) {
+            videoUri = Uri.parse(intent.getStringExtra("VIDEO_URI"));
+            Log.i("VIDEO_VIEW_TAG", "Video URI: " + videoUri);
+            videoView.setMediaController(mediaController);
+            videoView.setVideoURI(videoUri);
+            videoView.start();
+        }
+
+    }
+
+}
